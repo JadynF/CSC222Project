f70f8707b0c3df4829b9bed305d9e237a1f76b26
Jadyn F <jrf038@email.latech.edu>
2023-02-17 02:38:20 -0600

    Refactored, removed double loop

--- techshell.c
+++ techshell.c
@@ -2,6 +2,7 @@
 // Names: Jadyn Falls, Lael Hamilton
 // Date: 2/11/23
 // Desc: Simple shell with I/O redirections
+// and dynamicly allocated arguments
 ///////////////////////////////////////////
 #include <stdio.h>
 #include <unistd.h>
@@ -10,139 +11,121 @@
 #include <errno.h>
 #include <sys/wait.h>
 
+// function prototypes
+void printDir();
+char** parseInput(char input[], char* infile[], char* outfile[]);
+void execCommand(char** args, char* infile[], char* outfile[]);
+
+// main that loops for input and calls other functions
+int main(int argc, char argv[]) {
+	// strings for input, args, and redirection file names
+	char input[255];
+	char* infile[1];
+	char* outfile[1];
+	char** args = NULL;
+	printDir();
+	// loop to get input
+	while (fgets(input, 255, stdin) !=  NULL) {
+		// set strings to NULL
+		infile[0] = NULL;
+		outfile[0] = NULL;
+		args = NULL;
+		// exit case
+		if (!strcmp(input, "exit\n"))
+			break;
+		// get argument list and redirection files
+		args = parseInput(input, infile, outfile);
+		// execute arguments
+		execCommand(args, infile, outfile);
+		// print curr directory
+		printDir();
+	}
+	return 0;
+}
+
 // print out current working directory
 void printDir() {
 	char dir[100];
 	printf("%s$ ", getcwd(dir, 100));
 }
 
-// return the number of arguments in the input
-int inputLen(char input[]) {
-	// copy of input
-	char inputcpy[255];
-	strcpy(inputcpy, input);
-	char* tok = strtok(inputcpy, " ");
-	int count = 0;
-	// loop through input, count number of args
-	while (tok != NULL) {
-		tok = strtok(NULL, " ");
-		count++;
-	}
-	return count;
-}
-
-// parse the input by space and add arguments to list
-void parseInput(char input[], char *args[]) {
-	char* tok = strtok(input, " ");
+// parse the input, add arguments to list, and get redirection file names
+char** parseInput(char input[], char* infile[], char* outfile[]) {
+	// initial array of arguments
+	char** args = (char **) malloc(sizeof(char*));
+	// delimiters for tokens
+	char delim[] = " \n";
+	char* tok = strtok(input, delim);
 	int i = 0;
 	while (tok != NULL) {
-		args[i] = (char *) malloc((strlen(tok) + 1) * sizeof(char));
-		strcpy(args[i++], tok);
-		tok = strtok(NULL, " ");
+		// check for file redirections
+		if (!strcmp(tok, "<")) {
+			// move to next token
+			tok = strtok(NULL, delim);
+			// allocate space and save file name
+			infile[0] = (char *) malloc(sizeof(char*) * strlen(tok));
+			strcpy(infile[0], tok);
+		}
+		else if (!strcmp(tok, ">")) {
+			// move to next token
+			tok = strtok(NULL, delim);
+			// allocate space and save file name
+			outfile[0] = (char *) malloc(sizeof(char*) * strlen(tok));
+			strcpy(outfile[0], tok);
+		}
+		else {
+			// allocate for new element in argument list
+			args = (char **) realloc(args, sizeof(char*) * (i + 2));
+			// set element to token
+			args[i] = tok;
+			i++;
+		}
+		tok = strtok(NULL, delim);
 	}
-	// get rid of last 2 characters in last element (\n)
-	i--;
-	int len = strlen(args[i]);
-	args[i][len] = '\0';
-	args[i][len - 1] = '\0';
-	args[i + 1] = NULL;
+	// set end of args arr to null
+	args[i] = NULL;
+	return args;
 }
 
 // execute the input
-void execCommand(char *args[], char *infile[], char *outfile[]) {
-		// check if a chdir call
-		if (args[0] != NULL && strcmp(args[0], "cd")) {
-			pid_t pID = fork();
-			// child executes command
-			if (pID == 0) {
-				// if there is a file name for redirection
-				if (infile[0] != NULL) {
-					FILE* openedInfile = fopen(infile[0], "r");
-					// if file exists and for other errors
-					if (openedInfile == NULL) {
-						printf("Error %d (%s)\n", errno, strerror(errno));
-						exit(0);
-					}
-					dup2(fileno(openedInfile), 0);
-					fclose(openedInfile);
-				}
-				if (outfile[0] != NULL) {
-					FILE* openedOutfile = fopen(outfile[0], "w");
-					// check for error when opening file
-					if (openedOutfile == NULL) {
-						printf("Error %d (%s)\n", errno, strerror(errno));
-						exit(0);
-					}
-					dup2(fileno(openedOutfile), 1);
-					fclose(openedOutfile);
-				}
-				// execute command
-				if ((execvp(args[0], args)) == -1)
+void execCommand(char** args, char* infile[], char* outfile[]) {
+	// check if a chdir call
+	if (args[0] != NULL && strcmp(args[0], "cd")) {
+		pid_t pID = fork();
+		// child executes command
+		if (pID == 0) {
+			// if there is a file name for redirection
+			if (infile[0] != NULL) {
+				FILE* openedInfile = fopen(infile[0], "r");
+				// if file exists and for other errors
+				if (openedInfile == NULL) {
 					printf("Error %d (%s)\n", errno, strerror(errno));
-				exit(0);
+					exit(0);
+				}
+				dup2(fileno(openedInfile), 0);
+				fclose(openedInfile);
 			}
-			wait(0);
-		}
-		// if changing dir
-		else {
-			if (chdir(args[1]) != 0)
+			if (outfile[0] != NULL) {
+				FILE* openedOutfile = fopen(outfile[0], "w");
+				// check for error when opening file
+				if (openedOutfile == NULL) {
+					printf("Error %d (%s)\n", errno, strerror(errno));
+					exit(0);
+				}
+				dup2(fileno(openedOutfile), 1);
+				fclose(openedOutfile);
+			}
+			// execute command
+			if ((execvp(args[0], args)) == -1)
 				printf("Error %d (%s)\n", errno, strerror(errno));
+			exit(0);
 		}
-}
-
-// get the names of the redirection files
-void getReDirFiles(char* args[], char *infile[], char *outfile[]) {
-	int i = 0;
-	while (args[i] != NULL) {
-		// if redirecting into file
-		if (!strcmp(args[i], ">")) {
-			// set outfile to file name
-			outfile[0] = (char *) malloc(sizeof(char) * strlen(args[i + 1]));
-			strcpy(outfile[0], args[i + 1]);
-			// remove redirection from arguments
-			args[i] = NULL;
-			args[i + 1] = NULL;
-			i++;
-		}
-		// if input redirected from file
-		else if (!strcmp(args[i], "<")) {
-			// set infile to file name
-			infile[0] = (char*) malloc(sizeof(char) * strlen(args[i + 1]));
-			strcpy(infile[0], args[i + 1]);
-			// remove redirection from arguments
-			args[i] = NULL;
-			args[i + 1] = NULL;
-			i++;
-		}
-		i++;
+		wait(0);
+	}
+	// if changing dir
+	else {
+		if (chdir(args[1]) != 0)
+			printf("Error %d (%s)\n", errno, strerror(errno));
 	}
 }
 
-// main
-int main(int argc, char argv[]) {
-	// strings for input and redirection file names
-	char input[255];
-	char* infile[1];
-	char* outfile[1];
-	printDir();
-	// loop to get input
-	while (fgets(input, 255, stdin) !=  NULL) {
-		// exit case
-		if (!strcmp(input, "exit\n"))
-			break;
-		// set size of argument list
-		char* args[inputLen(input) + 1];
-		// parsed arguments by space and add to argument list
-		parseInput(input, args);
-		// get the names of redirection files
-		getReDirFiles(args, infile, outfile);
-		// execute arguments
-		execCommand(args, infile, outfile);
-		// print curr directory
-		printDir();
-		// reset file names
-		infile[0] = NULL;
-		outfile[0] = NULL;
-	}
-	return 0;
-}
